acea862edbf9e78f56bfd04c18f69cb6aecf530b
aizen1295 <david_forerope@fet.edu.co>
2019-08-24 11:19:01 -0500

    ejercicios

---
+++ respuesta.php
@@ -0,0 +1,21 @@
+<?php
+if($_POST){
+    $numero1= $_POST['num1'];
+    $numero2= $_POST['num2'];
+
+    if($numero1<$numero2){
+        echo"$numero1 es el mumero menor <br>";
+        echo " $numero2 es el numero mayor";
+    }
+    elseif($numero1>$numero2){
+        echo "$numero1 es el numero mayor <br>";
+        echo" $numero2 es el mumero menor";
+    }
+    else{
+        echo "los numeros $numero1 y $numero2 son iguales";
+    }
+}
+else{
+    echo"no hay post";
+}
+?>
